ac8886d3b6a9d631ca71b0453cc234f3b8ed615e
Thomas Gull <tgull1@dmail.dixie.edu>
2012-11-06 15:46:58 -0700

    Added Singleton AcctMgr

---
+++ Server/AccountManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+
+    public class AccountManager
+    {
+        private static AccountManager instance;
+
+        private AccountManager() { accounts = new List<Guid>(); }
+
+        private List<Guid> accounts;
+
+        public static AccountManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AccountManager();
+                }
+                return instance;
+            }
+        }
+
+        public void InsertAccount(Guid newAct)
+        {
+            accounts.Add(newAct);
+        }
+
+        public List<Guid> GetAccounts()
+        {
+            return accounts;
+        }
+    }
+}
